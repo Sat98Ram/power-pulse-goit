0170a8dcf5fd67ab7bc38ae22f7ca688e700581a
Iryna <irinkasatanovskaya@gmail.com>
2023-08-24 11:22:44 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./assets/global.css";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter basename="/power-pulse-goit">
+    <BrowserRouter basename="/power-pulse-goit">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );

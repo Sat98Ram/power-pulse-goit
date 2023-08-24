2c0a47acb974d10479bad2508a8a85d04d10e733
Iryna <irinkasatanovskaya@gmail.com>
2023-08-24 21:09:28 +0300

    update css userBar, header

--- src/components/layout/UserBar/UserBar.jsx
+++ src/components/layout/UserBar/UserBar.jsx
@@ -1,10 +1,13 @@
 import { NavLink } from "react-router-dom";
+import css from "./UserBar.module.css";
 
 const UserBar = () => {
   return (
-    <>
+    <div className={css.userBar}>
       <NavLink to="/profile">Profile</NavLink>
-    </>
+
+      <div className={css.avatarWrapper}></div>
+    </div>
   );
 };
 

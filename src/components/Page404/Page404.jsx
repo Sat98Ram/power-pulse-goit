8229432695177e5757a6d21ebca66115d719cc08
olegvoskovets <105343324+olegvoskovets@users.noreply.github.com>
2023-08-24 23:47:41 +0300

    add page 404

---
+++ src/components/Page404/Page404.jsx
@@ -0,0 +1,23 @@
+import css from "./Page404.module.css";
+
+const Page404 = () => {
+  return (
+    <div className={css.page404}>
+      <div className={css.left}>
+        <div className={css.container404}>
+          <h2>404</h2>
+          <h3>В РОЗРОБЦІ</h3>
+          <p>
+            Sorry, you have reached a page that we could not find. It seems that
+            you are lost among the numbers and letters of our virtual space.
+            Perhaps this page went on vacation or decided to disappear into
+            another dimension. We apologize for this inconvenience.
+          </p>
+        </div>
+      </div>
+      <div className={css.rigth}>fgbfgbfg</div>
+    </div>
+  );
+};
+
+export default Page404;

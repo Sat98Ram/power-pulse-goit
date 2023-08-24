740c5a9f4d41f8dc60873910be9b3348beafc869
Iryna <irinkasatanovskaya@gmail.com>
2023-08-24 11:19:18 +0300

    add UserNav

--- src/components/layout/UserNav/UserNav.jsx
+++ src/components/layout/UserNav/UserNav.jsx
@@ -1,12 +1,27 @@
 import { NavLink } from "react-router-dom";
+import css from "./UserNav.module.css";
 
 export const UserNav = () => {
   return (
     <>
       <nav>
-        <NavLink>Diary</NavLink>
-        <NavLink>Products</NavLink>
-        <NavLink>Exercises</NavLink>
+        <ul className={css.userNavList}>
+          <li className={css.userNavItem}>
+            <NavLink to="/diary" className={css.userNavLink}>
+              Diary
+            </NavLink>
+          </li>
+          <li className={css.userNavItem}>
+            <NavLink to="/products" className={css.userNavLink}>
+              Products
+            </NavLink>
+          </li>
+          <li className={css.userNavItem}>
+            <NavLink to="/exercises" className={css.userNavLink}>
+              Exercises
+            </NavLink>
+          </li>
+        </ul>
       </nav>
     </>
   );

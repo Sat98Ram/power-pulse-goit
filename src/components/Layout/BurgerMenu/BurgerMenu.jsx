2dfcb44b8926e20134efe9c2b5d2abf6525cef9f
Iryna <irinkasatanovskaya@gmail.com>
2023-08-30 17:26:33 +0300

    burger menu open/close

--- src/components/Layout/BurgerMenu/BurgerMenu.jsx
+++ src/components/Layout/BurgerMenu/BurgerMenu.jsx
@@ -1,28 +1,52 @@
 import { useState } from "react";
 
-// import symbolDefs from "../../../assets/images/symbol-defs.svg";
+import symbolDefs from "../../../assets/images/symbol-defs.svg";
 import css from "./BurgerMenu.module.css";
+import UserNav from "../UserNav/UserNav";
 
 const BurgerMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleIsOpen = () => {
-    const reverseIsOpen = isOpen;
+    setIsOpen(true);
+  };
 
-    setIsOpen(!reverseIsOpen);
+  const handleClosed = () => {
+    setIsOpen(false);
   };
 
   return (
-    <div
-      type="button"
-      className={isOpen ? css.menuBtn : css.openMenuBtn}
-      onClick={() => handleIsOpen()}
-    >
-      <div className={css.burgerMenuBtn}></div>
-      {/* <svg className={css.burgerIcon}>
-        <use href={symbolDefs + "#burger-menu-icon"}></use>
-      </svg> */}
-    </div>
+    <>
+      {!isOpen && (
+        <button
+          type="button"
+          className={css.menuBtn}
+          onClick={() => handleIsOpen()}
+          aria-label="open menu"
+        >
+          <svg className={css.burgerIcon}>
+            <use href={symbolDefs + "#burger-menu-icon"}></use>
+          </svg>
+        </button>
+      )}
+
+      {isOpen && (
+        <div className={css.burgerMenu}>
+          <button
+            type="button"
+            className={css.menuCloseBtn}
+            onClick={() => handleClosed()}
+            aria-label="close menu"
+          >
+            <svg className={css.closeMenuIcon}>
+              <use href={symbolDefs + "#close-icon"}></use>
+            </svg>
+          </button>
+
+          <UserNav />
+        </div>
+      )}
+    </>
   );
 };
 

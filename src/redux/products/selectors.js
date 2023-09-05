d7174186fd38b4ad04afbeacd68f73384b161dc4
olegvoskovets <105343324+olegvoskovets@users.noreply.github.com>
2023-09-05 22:21:29 +0300

    smooth opening of the menu

--- src/redux/products/selectors.js
+++ src/redux/products/selectors.js
@@ -1,5 +1,7 @@
-export const selectProductsList = (state) => state.products.list
+export const selectProductsList = (state) => state.products.list;
 
-export const selectCategoriesProducts = (state) => state.products.categories
+export const selectCategoriesProducts = (state) => state.products.categories;
 
-export const selectFilter = (state) => state.products.filter 
+export const selectFilter = (state) => state.products.filter;
+
+export const selectIsLoadingProduct = (state) => state.products.isLoading;

faea7009805666630dc5987033a4dda46b872fd7
Iryna <irinkasatanovskaya@gmail.com>
2023-09-06 10:46:50 +0300

    delete unnnecessary import

--- src/components/AddProductModalWindow/AddProductModalWindow.jsx
+++ src/components/AddProductModalWindow/AddProductModalWindow.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import PropTypes from "prop-types";
 
 import css from "./AddProductModalWindow.module.css";
--- src/components/ExersiceModalWindow/ExersiceModalWindow.jsx
+++ src/components/ExersiceModalWindow/ExersiceModalWindow.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { toast } from "react-toastify";
 import PropTypes from "prop-types";
 

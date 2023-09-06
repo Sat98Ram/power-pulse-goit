1ecc528fe9e49f10e79ec87a0ed8bb4ebaedfd2a
Iryna <irinkasatanovskaya@gmail.com>
2023-09-06 15:29:13 +0300

    Update UserNavItem.jsx

--- src/components/Layout/UserNav/UserNavItem.jsx
+++ src/components/Layout/UserNav/UserNavItem.jsx
@@ -1,10 +1,16 @@
 import PropTypes from "prop-types";
 import { NavLink } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 
 const UserNavItem = ({ link, label, className, classNameItem, setIsOpen }) => {
+  const isDesktop = useMediaQuery({ query: "(min-width: 1440px)" });
   return (
     <li className={classNameItem}>
-      <NavLink to={link} className={className} onClick={() => setIsOpen()}>
+      <NavLink
+        to={link}
+        className={className}
+        onClick={isDesktop ? () => setIsOpen() : null}
+      >
         {label}
       </NavLink>
     </li>

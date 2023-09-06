e6abd2b0aeafb71c9a651a2f094d7fd703c8e9a4
Iryna <irinkasatanovskaya@gmail.com>
2023-09-06 12:14:48 +0300

    Loader fix

--- src/components/Layout/Layout/Layout.jsx
+++ src/components/Layout/Layout/Layout.jsx
@@ -1,23 +1,32 @@
+import { useSelector } from "react-redux";
+import { Outlet } from "react-router-dom";
+
+import { selectIsAuth, selectisLoading } from "../../../redux/auth/selectors";
+import css from "./Layout.module.css";
 import Header from "../Header/Header";
 import VideoCount from "../../VideoCount";
 import CaloriesCount from "../../CaloriesCount";
-import { Outlet } from "react-router-dom";
-import css from "./Layout.module.css";
-import { useSelector } from "react-redux";
-import { selectIsAuth } from "../../../redux/auth/selectors";
+import Loader from "../../Loader/Loader";
+
 export const Layout = () => {
   const isAuth = useSelector(selectIsAuth);
+  const isLoading = useSelector(selectisLoading);
 
   return (
     <>
-      <Header />
+      {isLoading && <Loader />}
+      {!isLoading && (
+        <>
+          <Header />
 
-      <Outlet />
-      {!isAuth && (
-        <div className={css.statLine}>
-          <VideoCount />
-          <CaloriesCount />
-        </div>
+          <Outlet />
+          {!isAuth && (
+            <div className={css.statLine}>
+              <VideoCount />
+              <CaloriesCount />
+            </div>
+          )}
+        </>
       )}
     </>
   );

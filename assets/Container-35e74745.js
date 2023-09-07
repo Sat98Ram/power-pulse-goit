739859498354ac3cee3e0b355ddfa9fea6495cec
Iryna <irinkasatanovskaya@gmail.com>
2023-09-07 12:33:25 +0300

    Updates

---
+++ assets/Container-35e74745.js
@@ -0,0 +1,1 @@
+import{P as n,j as r}from"./index-53f86240.js";const e="_container_wdz1c_1",t={container:e},a=({children:s,className:o=""})=>r.jsx("div",{className:`${t.container} ${o}`,children:s});a.propTypes={children:n.node,className:n.string};export{a as C};
---
+++ assets/TitlePage-8269c454.js
@@ -0,0 +1,1 @@
+import{P as t,j as s}from"./index-53f86240.js";const p="_title_page_11v7q_1",i={title_page:p},r=({text:e})=>s.jsx("h2",{className:i.title_page,children:e});r.propTypes={text:t.string.isRequired};export{r as T};
